4275bbd6bdd831a84bc09bd5663f822d10b49dc3
ivan-martinm <ivan87.martin@gmail.com>
2022-06-20 15:01:16 +0200

    added part1/unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) =>
+const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
+)
 
 const Statistics = ({ good, neutral, bad }) => {
   let total = good + neutral + bad
@@ -18,21 +19,26 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={(good - bad) / total} />
-      <StatisticLine text="positive" value={(good / total) * 100 + " %"} />
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={(good - bad) / total} />
+        <StatisticLine text="positive" value={(good / total) * 100 + " %"} />
+        </tbody>
+      </table>
     </div>
   )
 }
 
-const StatisticLine = ({text, value}) => {
-  return (
-    <p>{text} {value}</p>
-  )
-}
+const StatisticLine = ({ text, value }) => (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+)
 
 const App = () => {
   // save clicks of each button to its own state
