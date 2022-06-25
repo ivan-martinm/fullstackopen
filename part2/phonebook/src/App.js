f1d3f568b6b7e9d0e8bd98cc0ed61ea9e74934bd
ivan-martinm <ivan87.martin@gmail.com>
2022-06-25 09:40:12 +0200

    added part2/phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const Filter = ({ searchTerm, handleSearchTermChange }) =>
   <div>
@@ -34,27 +35,19 @@ const Person = ({ person }) =>
   <div>{person.name} {person.number}</div>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '39-44-5323523'
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345'
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122'
-    }
-  ])
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect (() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
