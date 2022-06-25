7d275eaaa625decb0696385e89a47f29cc4168e0
ivan-martinm <ivan87.martin@gmail.com>
2022-06-25 14:04:04 +0200

    added part2/countries step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-const Countries = ({ countries, searchTerm }) => {
+const Filter = ({ searchTerm, handleSearchTerm }) => {
+  return (
+    <div>
+      find countries
+      <input value={searchTerm} onChange={handleSearchTerm} />
+    </div>
+  )
+}
+
+const Countries = ({ countries, searchTerm, setSearchTerm }) => {
   const filteredCountries =
     countries.filter(country => country.name.common.toLowerCase()
       .includes(searchTerm.toLowerCase()))
@@ -10,25 +19,33 @@ const Countries = ({ countries, searchTerm }) => {
     return <div>Too many matches, specify another filter</div>
   }
   else if (filteredCountries.length > 1) {
-    return filteredCountries.map(country => <div key={country.name.common}>{country.name.common}</div>)
-  }
-  else if (filteredCountries.length === 1) {
-    const country = filteredCountries[0]
-    return (
-      <>
-        <h2>{country.name.common}</h2>
-        <div>
-          <div>capital {country.capital}</div>
-          <div>area {country.area}</div>
-        </div>
-        <p><strong>languages:</strong></p>
-        <ul>
-          {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-        </ul>
-        <img src={country.flags.png} alt="Flag" width="20%" border="1px" />
-      </>
+    return filteredCountries.map(country =>
+      <div key={country.name.common}>
+        {country.name.common}
+        <button onClick={() => setSearchTerm(country.name.common)}>show</button>
+      </div>
     )
   }
+  else if (filteredCountries.length === 1) {
+    return <Country country={filteredCountries[0]} />
+  }
+}
+
+const Country = ({ country }) => {
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      <div>
+        <div>capital {country.capital}</div>
+        <div>area {country.area}</div>
+      </div>
+      <p><strong>languages:</strong></p>
+      <ul>
+        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+      </ul>
+      <img src={country.flags.png} alt="Flag" width="150px" border="1px"/>
+    </>
+  )
 }
 
 const App = () => {
@@ -46,11 +63,10 @@ const App = () => {
   }
 
   return (
-    <div>
-      find countries
-      <input value={searchTerm} onChange={handleSearchTerm} />
-      <Countries countries={countries} searchTerm={searchTerm} />
-    </div>
+    <>
+      <Filter searchTerm={searchTerm} handleSearchTerm={handleSearchTerm} />
+      <Countries countries={countries} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    </>
   )
 }
 
