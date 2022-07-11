f5accf133e63b5f38bad6f0d3900ea92e77b29e7
ivan-martinm <ivan87.martin@gmail.com>
2022-07-11 12:53:54 +0200

    added part5/bloglist-frontend step1

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
+  return (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>username
+          <input type="text" name="username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>password
+          <input type="password" name="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      console.log('logged in')
+      setUser(user)
+      setUsername()
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  return (
+    user === null
+      ? <LoginForm handleLogin={handleLogin} username={username}
+        setUsername={setUsername} password={password}
+        setPassword={setPassword} />
+      : <div><h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+  )
+}
+
+export default App
