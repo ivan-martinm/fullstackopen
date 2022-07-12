83931edb3af3e35784c67ccad6b6bc50cd99e1dd
ivan-martinm <ivan87.martin@gmail.com>
2022-07-12 14:02:30 +0200

    added part5/bloglist-frontend step5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import NewBlogForm from './components/NewBlogForm'
+import Toggleable from './components/Toggleable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -17,6 +18,8 @@ const App = () => {
   const [url, setUrl] = useState('')
 
   const [message, setMessage] = useState(null)
+
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -78,6 +81,7 @@ const App = () => {
       setTimeout(() => { setMessage(null) }, 5000)
       return logout()
     }
+    blogFormRef.current.toggleVisibility()
     newBlog = {
       id: response.data.id,
       title: response.data.title,
@@ -94,17 +98,20 @@ const App = () => {
     <>
       <Notification message={message} />
       {user === null
-        ? <LoginForm handleLogin={handleLogin} username={username}
+        ?
+        <LoginForm handleLogin={handleLogin} username={username}
           setUsername={setUsername} password={password}
           setPassword={setPassword} />
         : <div><h2>blogs</h2>
           <p>{user.name} logged in
             <button onClick={handleLogout}>log out</button>
           </p>
-          <NewBlogForm title={title} author={author}
-            url={url} setTitle={setTitle}
-            setAuthor={setAuthor} setUrl={setUrl}
-            handleNewBlog={handleNewBlog} />
+          <Toggleable buttonLabel="new blog" ref={blogFormRef}>
+            <NewBlogForm title={title} author={author}
+              url={url} setTitle={setTitle}
+              setAuthor={setAuthor} setUrl={setUrl}
+              handleNewBlog={handleNewBlog} />
+          </Toggleable>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
