56b30c0adb6d6d40d151465d36d336e7da1c6ded
ivan-martinm <ivan87.martin@gmail.com>
2022-07-13 15:56:47 +0200

    added part5/bloglist-frontend tests step2

--- part5/bloglist-frontend/src/tests/Blog.test.js
+++ part5/bloglist-frontend/src/tests/Blog.test.js
@@ -1,25 +1,43 @@
 import Blog from '../components/Blog'
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-test('Blog renders title and author by default but does not render url and likes', () => {
-  const blog = {
-    title: 'This is the title',
-    author: 'Anonymous',
-    url: 'http://localhost',
-    likes: 13,
-    user: {
-      username: 'user01',
-      name: 'User 01',
-    }
-  }
+describe('testing Blog component', () => {
+  let container
 
-  const { container } = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    container = render(
+      <Blog blog={
+        {
+          title: 'This is the title',
+          author: 'Anonymous',
+          url: 'http://localhost',
+          likes: 13,
+          user: {
+            username: 'user01',
+            name: 'User 01',
+          }
+        }
+      } />).container
+  })
 
-  let div = container.querySelector('.fixedContent')
-  expect(div).toHaveTextContent('This is the title Anonymous')
+  test('Blog renders title and author by default but does not render url and likes', () => {
+    let div = container.querySelector('.fixedContent')
+    expect(div).toHaveTextContent('This is the title Anonymous')
 
-  div = container.querySelector('.toggleableContent')
-  expect(div).toHaveStyle('display: none')
+    div = container.querySelector('.toggleableContent')
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('Url and likes are rendered after show button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.toggleableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
+
 })
