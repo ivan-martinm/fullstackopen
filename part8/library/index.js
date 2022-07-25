a95f9796ddf856b00b963ca182d6c6a1fdfb98fe
ivan-martinm <ivan87.martin@gmail.com>
2022-07-25 12:10:21 +0200

    added part8/library all authors

--- part8/library/index.js
+++ part8/library/index.js
@@ -97,10 +97,18 @@ const typeDefs = gql`
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]
+    allAuthors: [Author!]
   }
 `
 
@@ -109,7 +117,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors
   },
+  Author: {
+    bookCount: (root) => {
+      const booksFound = books.filter(book => book.author === root.name)
+      return booksFound.length
+    }
+  }
 }
 
 const server = new ApolloServer({
